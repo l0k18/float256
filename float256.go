6e72428f6fac371e98c1d0e1a43c66b29ad08515
Loki Verloren <l0k1@null.net>
2018-05-17 23:41:08 +0300

    Added limit checks for exponent size and informative godoc comments

--- float256.go
+++ float256.go
@@ -1,11 +1,13 @@
-// Package float256 implements a 256 bit precision floating point math library for high precision financial calculations
+// Package float256 is a high precision fixed point library built from math/big's big.Float with a fixed maximum precision of 42 integer places and precision to 63 decimal places intended for use in a cryptocurrency ledger for a token with a maximum supply less than 42 bits of integer place token denomination of total supply. The maximum value the integer part can hold is 4,398,046,511,103.
+// It has a serialisation codec that stores this as a 42.214 fixed precision value and converts it back to big.Float.
+// During calculations it is possible to work with negative numbers, for convenience, however they cannot be imported using strings or integers or encoded and the functions return nil to signify the input is invalid.
 package float256
 
 import (
 	"math/big"
 )
 
-// Zero returns a new float with 256 bits of precision
+// Zero returns a new float with 256 bits of precision. This can be a good way to generate a new variable with minimal typing for input conversion functions particularly.
 func Zero() *big.Float {
 	r := big.NewFloat(0.0)
 	r.SetPrec(256)
@@ -19,12 +21,13 @@ func New(f float64) *big.Float {
 	return r
 }
 
-// FromString returns a float with 256 bit precision from a string or zero if invalid
-// The reverse conversion is built into big.Float with .String() though it is unnecessary to do this to use with any of the fmt Print* functions
+// FromString accepts a properly formed string of integer or decimal value and returns a big.Float to the caller. Negative values and values greater than 4,398,046,511,103 are invalid and rejected by returning nil.
 func FromString(s string) *big.Float {
-	r, _ := Zero().SetString(s)
-	r.SetPrec(256)
-	return r
+	r, success := Zero().SetString(s)
+	if r.Sign()<0 { return nil }
+	if r.MantExp(nil)>42 { return nil }
+	if success { return r }
+	return nil
 }
 
 // Int returns the truncated value of the parameter as a *big.Int
@@ -33,9 +36,12 @@ func Int(a *big.Float) *big.Int {
 	return i
 }
 
-// FromInt returns a *big.Float from a *big.Int
+// FromInt returns a *big.Float from a *big.Int. Negative values and values greater than 4,398,046,511,103 are invalid and rejected by returning nil.
 func FromInt(i *big.Int) *big.Float {
-	return Zero().SetInt(i)
+	r := Zero().SetInt(i)
+	if r.Sign()<0 { return nil }
+	if r.MantExp(nil)>42 { return nil }
+	return r
 }
 
 // Int64 returns the value of the parameter as an int64 with the decimal truncated
@@ -46,9 +52,12 @@ func Int64(a *big.Float) int64 {
 	return i
 }
 
-// FromInt64 returns a *big.Float from an int64
+// FromInt64 returns a *big.Float from an int64. Negative values and values greater than 4,398,046,511,103 are invalid and rejected by returning nil.
 func FromInt64(i int64) *big.Float {
-	return Zero().SetInt64(i)
+	r := Zero().SetInt64(i)
+	if r.Sign()<0 { return nil }
+	if r.MantExp(nil)>42 { return nil}
+	return r
 }
 
 // Uint64 returns the value of the parameter as a uint64 with decimal truncated and sign positive (obviously)
@@ -59,9 +68,12 @@ func Uint64(a *big.Float) uint64 {
 	return i
 }
 
-// FromUint64 returns a *big.Float from a uint64
+// FromUint64 returns a *big.Float from a uint64. Values greater than 4,398,046,511,103 are invalid and rejected by returning nil as they exceed the precision of the serialization format.
 func FromUint64(i uint64) *big.Float {
-	return Zero().SetUint64(i)
+	r := Zero().SetUint64(i)
+	if r.Sign()<0 { return nil }
+	if r.MantExp(nil)>42 { return nil }
+	return r
 }
 
 // Equal returns true if x and y are equal
@@ -88,9 +100,9 @@ func Exp(a *big.Float, e uint64) *big.Float {
 	return result
 }
 
-// Root returns the nth root of a
+// Root returns the nth root of a to 255 significant bits (if it is larger this function gets stuck in an infinite loop)
 func Root(a *big.Float, n uint64) *big.Float {
-	limit := Exp(New(2), 256)
+	limit := Exp(New(2), 255)
 	n1 := n-1
 	n1f, rn := New(float64(n1)), Div(New(1.0), New(float64(n)))
 	x, x0 := New(1.0), Zero()
@@ -114,7 +126,7 @@ func Abs(a *big.Float) *big.Float {
 	return Zero().Abs(a)
 }
 
-// Neg flips the sign of the parameter Float
+// Neg flips the sign of the parameter Float. Negative values are rejected by the Encode function as they are invalid in a cryptocurrency ledger.
 func Neg(a *big.Float) *big.Float {
 	return a.Neg(a)
 }
@@ -158,4 +170,20 @@ func Mod(a, b *big.Float) *big.Float {
 	return rem
 }
 
-// TODO: write codec to convert to and from 256 bit fixed point with zero padding and no sign
+// Encode takes a big.Float, left shifts and truncates to 214 bits of decimal precision and maximum 42 bits of integer precision and returns a byte slice that can be stored on disk or sent over a network. Negative values and values greater than 4,398,046,511,103 are invalid and rejected by returning nil.
+func Encode(a *big.Float) []byte {
+	if a.Sign()<0 { return nil }
+	mantissa := New(0)
+	a.MantExp(mantissa)
+	exp := a.MantExp(nil)
+	if exp > 42 { return nil }
+	bytes := Int(Mul(Exp(New(2), uint64(214+exp)), mantissa)).Bytes()
+	return append(make([]byte, 32-len(bytes)), bytes...)
+}
+
+// Decode takes a value that has been created by Encode, loads it into a big.Float, changes the exponent to shift the point to 214 bits of decimal precision and 42 bits of integer precision (exponent)
+func Decode(b []byte) *big.Float {
+	decoded := big.NewInt(0).SetBytes(b)
+	decodedF := Zero().SetInt(decoded)
+	return Zero().SetMantExp(decodedF, -214)
+}
